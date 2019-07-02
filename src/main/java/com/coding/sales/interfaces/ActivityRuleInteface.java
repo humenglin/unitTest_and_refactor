9501e365f19ae03cbe9b87ed1790ebeea4ace07a
ouzhaohui <359634368@qq.com>
2019-07-02 22:41:08 +0800

    feat:添加活动规则处理接口

---
+++ src/main/java/com/coding/sales/interfaces/ActivityRuleInteface.java
@@ -0,0 +1,13 @@
+package com.coding.sales.interfaces;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Administrator
+ *活动规则接口
+ */
+public interface ActivityRuleInteface {
+
+	public abstract BigDecimal activityRule(BigDecimal price,BigDecimal goodsAmount, BigDecimal discountOfactivity,String activityType) ;
+
+}
