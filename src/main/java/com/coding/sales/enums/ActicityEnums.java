0ac164fccbdc49dc13e4fe40f09a1eb1163ad484
ouzhaohui <359634368@qq.com>
2019-07-02 18:36:43 +0800

    feat:添加枚举类

---
+++ src/main/java/com/coding/sales/enums/ActicityEnums.java
@@ -0,0 +1,22 @@
+package com.coding.sales.enums;
+/*活动枚举值*/
+public enum ActicityEnums {
+	/*满足300减350*/
+	activity_3000,
+	/*满足2000减少30*/
+	activity_2000,
+	/*满足1000减10*/
+	activity_1000,
+	/*满3件减少一半*/
+	activity_3_half,
+	/*满3送1*/
+	activity_3_send_one,
+	/*打9折*/
+	voucher_9,
+	/*打95折*/
+	voucher_95;
+	
+	
+	
+
+}
